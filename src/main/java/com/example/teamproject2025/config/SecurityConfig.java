6dea3d71695dc6378a885ee973a358f8c68e2bb4
Hooby <hooby11105@naver.com>
2025-02-09 00:17:42 +0900

    Add: SecurityConfig and relevant Libraries

---
+++ src/main/java/com/example/teamproject2025/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.teamproject2025.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers("/**").permitAll() // 모든 URL 접속 허용
+        );
+        return http.build();
+    }
+}
