3fe1458cfec07ec2d7093f3cac9fce9468a0e53d
ChaeHyunLim <chaehyun010104@gmail.com>
2025-02-17 17:52:33 +0900

    merge sucess dev_seohyeon

--- frontend/src/pages/UpdateProfilePage.js
+++ frontend/src/pages/UpdateProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getUserProfile, updateUserProfile } from "../api/userApi";
 import { useNavigate } from "react-router-dom";
+import EmailVerificationForm from '../components/EmailVerificationForm';
 
 const UpdateProfilePage = () => {
     const navigate = useNavigate();
