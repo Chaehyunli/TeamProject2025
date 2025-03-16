ad0956fb38ba00f24af42c4e63e588cd4abe69d0
Hooby <hooby11105@naver.com>
2025-03-16 11:05:12 +0900

    fix: TestPage.js return jsx

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -9,7 +9,7 @@ const TestPage = () => {
     const [error, setError] = useState(null);
 
     // ✅ 개발자가 지정한 테스트용 회장 ID
-    const targetUserId = 1;
+    const targetUserId = 2;
 
     useEffect(() => {
         const loadUsers = async () => {
@@ -39,23 +39,26 @@ const TestPage = () => {
         return <div className="p-10">⏳ 회원 목록을 불러오는 중...</div>;
     }
 
-    if (error) {
-        return <div className="p-10 text-red-500">{error}</div>;
-    }
+    // if (error) {
+    //     return (
+    //         <div className="p-6 py-[120px]">
+    //             <p className="text-red-500">❌ 해당 ID를 가진 사용자를 찾을 수 없습니다.</p>
+    //         </div>
+    //     );
+    // }
 
     return (
         <div className="p-6 py-[120px]">
             <h1 className="text-2xl font-bold">1:1 문의하기 테스트</h1>
-            {president ? (
-                <>
-                    <p>테스트 계정: {president.name} </p>
-                    <DirectMessageButton presidentId={president.userId} receiverName={president.name}/>
-                </>
+            {error || !president ? (
+                <p className="text-red-500 py-[20px]">❌ 해당 ID를 가진 사용자를 찾을 수 없습니다.</p>
             ) : (
-                <p className="text-red-500">❌ 해당 ID를 가진 사용자를 찾을 수 없습니다.</p>
+                <>
+                    <p>테스트 계정: {president.name}</p>
+                    <DirectMessageButton presidentId={president.userId} receiverName={president.name} />
+                    <UserChatList />
+                </>
             )}
-
-            <UserChatList />
         </div>
     );
 };
