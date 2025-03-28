43a3f22a94ca26f62eb7dba970eb2caad1f177e5
DOOYEE0709 <hyeonta03@gmail.com>
2025-03-28 17:25:52 +0900

    style: clean up commented code in LoginForm component

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../api/authApi";
 import { getUserProfile } from "../api/userApi";
@@ -8,7 +7,6 @@ import { useAuth } from "../context/AuthContext";
 
 const LoginForm = () => {
     const navigate = useNavigate(); // 페이지 이동을 위한 훅
-    // const [storedUsername, setStoredUsername] = useState(localStorage.getItem("username") || null); // 초기값을 null로 설정
     const { loginUser } = useAuth();
     const [formData, setFormData] = useState({ username: "", password: "" });
     const [message, setMessage] = useState("");
@@ -44,13 +42,6 @@ const LoginForm = () => {
             setMessage("오류 발생: " + error.message);
         }
     };
-
-    // useEffect(() => {
-    //     // 로그인 상태 확인 후 자동 이동
-    //     if (storedUsername) {
-    //         navigate("/home");
-    //     }
-    // }, [storedUsername]);
 
     return (
         <form onSubmit={handleSubmit} className="space-y-4">
