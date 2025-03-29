35a2c9beea59e4c7e4d1a12d47282ae7c7749dea
Hooby <hooby11105@naver.com>
2025-03-29 22:19:45 +0900

    fix: ChatMessageRepo, BanUserRepo for countBadMessagesByUser Method

--- src/main/java/com/example/teamproject2025/repository/User/BanUserRepository.java
+++ src/main/java/com/example/teamproject2025/repository/User/BanUserRepository.java
@@ -3,10 +3,9 @@ package com.example.teamproject2025.repository.User;
 import com.example.teamproject2025.entity.User.BanUser;
 import com.example.teamproject2025.entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BanUserRepository extends JpaRepository<BanUser, Long> {
-    @Query("SELECT COUNT(m) FROM ChatMessage m WHERE m.user = :user AND m.isBadWord = true")
-    Integer countBadMessagesByUser(@Param("user") User user);
+    Optional<BanUser> findByUser(User user);
 }
