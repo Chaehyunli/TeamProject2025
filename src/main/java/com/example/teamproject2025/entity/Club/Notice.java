2d2a2dc72ff527505167f15d1ce4be46007d9e22
DOOYEE0709 <hyeonta03@gmail.com>
2025-04-01 01:45:31 +0900

    refactor: simplify and clarify column names in Notice table

--- src/main/java/com/example/teamproject2025/entity/Club/Notice.java
+++ src/main/java/com/example/teamproject2025/entity/Club/Notice.java
@@ -19,29 +19,29 @@ public class Notice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long noticeId;
+    private Long noticeId;    // 공지사항 고유 id
 
     @Column(nullable = false)
-    private String noticeContents;
+    private String title;   // 공지사항 제목
 
-    @Column(nullable = false)
-    private String noticeTitle;
+    @ManyToOne
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;  // 공지사항 작성할 동아리
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "club_id", nullable = false)
-    private Club club;
+    @Column(nullable = false)
+    private String contents; // 공지사항 본문
 
-    @Column(nullable = true, columnDefinition = "TEXT")
-    private String thumbUrl;  // 게시물 사진 URL (선택 사항)
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String thumbUrl;  // 공지사항 사진 URL (선택 사항)
 
     @CreationTimestamp
-    private LocalDateTime createdAt;    // 게시물 작성 시간
+    private LocalDateTime createdAt;    // 공지사항 작성 시간
 
     @UpdateTimestamp
-    private LocalDateTime updatedAt;    // 게시물 수정 시간
+    private LocalDateTime updatedAt;    // 공지사항 수정 시간
 
 }
