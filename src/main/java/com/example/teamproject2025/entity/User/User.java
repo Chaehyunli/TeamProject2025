6c7a6d2fe96bc56f7f82e49192016ea83bac9618
Hooby <hooby11105@naver.com>
2025-04-13 02:37:11 +0900

    remove: isUniVerified fields

--- src/main/java/com/example/teamproject2025/entity/User/User.java
+++ src/main/java/com/example/teamproject2025/entity/User/User.java
@@ -16,12 +16,13 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
-//@Table(name = "users", uniqueConstraints = {
-//        @UniqueConstraint(name = "users_email_unique", columnNames = "email"),
-//        @UniqueConstraint(name = "users_username_unique", columnNames = "username"),
-//        @UniqueConstraint(name = "users_studentId_unique", columnNames = "student_id")
-//}) // 이미 위 항목들이 unique = true로 설정이되어 있기 때문에 자동적으로 인덱스를 생성함. 이를 또 생성하는 것은 중복 인덱스를 생성하는 것이기 때문에 삭제!
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(name = "users_email_unique", columnNames = "email"),
+        @UniqueConstraint(name = "users_username_unique", columnNames = "username"),
+        @UniqueConstraint(name = "users_studentId_unique", columnNames = "student_id")
+})
+// 단일 컬럼의 unique=true 를 없애면 될 것 같아요.
+// 이건 Exception Handler 에서 식별하려면 저 name 이 필요합니다. 저것도 똑같이 unique 거는거니까 상관없어요.
 public class User {
 
     @Id
@@ -31,7 +32,7 @@ public class User {
     @Column(nullable = true)
     private Long universityId; // 소속 대학교 ID (nullable, foreign key)
 
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(nullable = false, length = 50)
     private String username; // 사용자 아이디
 
     @Column(nullable = false, length = 255)
@@ -40,17 +41,16 @@ public class User {
     @Column(nullable = false, length = 100)
     private String name; // 사용자 이름
 
-    @Column(nullable = false, unique = true, length = 20)
+    @Column(nullable = false, length = 20)
     private String studentId; // 학번 (eg. 60225153)
 
     @Column(nullable = true, length = 255)
     private String department; // 전공 학과 (nullable)
 
-    @Column(nullable = false, unique = true, length = 100)
+    @Column(nullable = false, length = 100)
     private String email; // 이메일 주소
 
     private Boolean isEmailVerified ; // 이메일 인증 여부
-    private Boolean isUniVerified ; // 학교 인증 여부
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String profileImage;
